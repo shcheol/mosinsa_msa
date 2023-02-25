ed09436204e4c9d0b7d37cf1b482712375f91f7e
shcheol <kian1523@naver.com>
2023-02-25 17:01:48 +0900

    create entity classes

---
+++ src/main/java/com/shopping/mosinsa/entity/CouponStatus.java
@@ -0,0 +1,5 @@
+package com.shopping.mosinsa.entity;
+
+public enum CouponStatus {
+    USED, UNUSED
+}
---
+++ src/main/java/com/shopping/mosinsa/entity/CustomerGrade.java
@@ -0,0 +1,6 @@
+package com.shopping.mosinsa.entity;
+
+public enum CustomerGrade {
+
+    BRONZE, SILVER, GOLD, PLATINUM
+}
