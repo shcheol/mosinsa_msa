a3d2bcdbbd1af64024d4a8f84ae30f3c459a9dc9
shcheol <kian1523@naver.com>
2023-03-04 18:37:11 +0900

    product 완료

---
+++ src/test/java/com/shopping/mosinsa/ApiTest.java
@@ -0,0 +1,28 @@
+package com.shopping.mosinsa;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Transactional
+public class ApiTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    DatabaseCleanup databaseCleanup;
+
+    @BeforeEach
+    void setUp(){
+        if(RestAssured.port == RestAssured.UNDEFINED_PORT) {
+            RestAssured.port = port;
+            databaseCleanup.afterPropertiesSet();
+        }
+        databaseCleanup.execute();
+    }
+}
