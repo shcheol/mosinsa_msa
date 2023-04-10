40fc3a925305ef0406c40c8f83726c22e9bd2db9
shcheol <kian1523@naver.com>
2023-04-10 19:18:31 +0900

    [dev] redis 연동 및 동작 테스트

--- src/main/java/com/shopping/mosinsa/controller/CouponController.java
+++ src/main/java/com/shopping/mosinsa/controller/CouponController.java
@@ -8,6 +8,8 @@ import com.shopping.mosinsa.entity.Coupon;
 import com.shopping.mosinsa.entity.CouponEvent;
 import com.shopping.mosinsa.service.CouponService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +21,7 @@ public class CouponController {
 
     private final CouponService couponService;
 
+    private final RedisTemplate<Long, Long> redisTemplate;
 
     @GetMapping("/event/{id}")
     public ResponseEntity<CouponEventDto> createEvent(@PathVariable Long id){
@@ -44,4 +47,20 @@ public class CouponController {
         return ResponseEntity.status(HttpStatus.OK).body(new CouponDto(coupon));
     }
 
+    @PatchMapping("/redis")
+    public ResponseEntity<Void> couponIssuanceRequest(@RequestBody CouponIssuanceRequest request){
+        ValueOperations<Long, Long> value = redisTemplate.opsForValue();
+        Long customerId = request.getCustomerId();
+        Long couponEventId = request.getCouponEventId();
+        value.set(customerId,couponEventId);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/redis/{key}")
+    public ResponseEntity<?> getRedisKey(@PathVariable String key) {
+        ValueOperations<Long, Long> vop = redisTemplate.opsForValue();
+        Long value = vop.get(key);
+        return new ResponseEntity<>(value, HttpStatus.OK);
+    }
 }
