c770ace0e0bb5d400ba12b857edd825209b0c9cf
shcheol <kian1523@naver.com>
2023-06-22 22:29:09 +0900

    [product] 주문 취소시 수량 동기화

--- product/src/main/java/com/mosinsa/product/messegequeue/KafkaConsumer.java
+++ product/src/main/java/com/mosinsa/product/messegequeue/KafkaConsumer.java
@@ -1,8 +1,6 @@
 package com.mosinsa.product.messegequeue;
 
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mosinsa.product.dto.OrderProductDto;
 import com.mosinsa.product.entity.Product;
@@ -13,12 +11,6 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -26,12 +18,13 @@ public class KafkaConsumer {
 
     private final ProductRepository repository;
 
+    private final ObjectMapper om;
+
     @Transactional
-    @KafkaListener(topics = "mosinsa-product-topic")
-    public void updateQuantity(String kafkaMessage){
+    @KafkaListener(topics = "mosinsa-product-order")
+    public void orderProduct(String kafkaMessage){
         log.info("kafka message: {}", kafkaMessage);
 
-        ObjectMapper om = new ObjectMapper();
         OrderProductDto orderProductDto = null;
         try{
             orderProductDto = om.readValue(kafkaMessage, OrderProductDto.class);
@@ -47,8 +40,29 @@ public class KafkaConsumer {
         log.info("orderCount = {}", orderCount);
         Product product = repository.findById(orderProductDto.getProductDto().getProductId()).orElseThrow(() -> new IllegalArgumentException("조회한 상품이 없습니다."));
 
-        log.info("product: {}", product.toString());
         product.removeStock(orderCount);
-        log.info("product: {}", product.toString());
+    }
+
+    @Transactional
+    @KafkaListener(topics = "mosinsa-product-cancel")
+    public void cancelOrder(String kafkaMessage){
+        log.info("kafka message: {}", kafkaMessage);
+
+        OrderProductDto orderProductDto = null;
+        try{
+            orderProductDto = om.readValue(kafkaMessage, OrderProductDto.class);
+            log.info("{}",orderProductDto);
+
+        }catch (JsonProcessingException e){
+            e.printStackTrace();
+        }
+
+        assert orderProductDto != null;
+        int orderCount = orderProductDto.getOrderCount();
+
+        log.info("orderCount = {}", orderCount);
+        Product product = repository.findById(orderProductDto.getProductDto().getProductId()).orElseThrow(() -> new IllegalArgumentException("조회한 상품이 없습니다."));
+
+        product.addStock(orderCount);
     }
 }
