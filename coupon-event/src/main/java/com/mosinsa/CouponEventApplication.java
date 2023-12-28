4f3d8d0c761c82564cb4c171705116e41566b940
unknown <kian1523@naver.com>
2023-12-28 15:58:17 +0900

    fix

--- coupon-event/src/main/java/com/mosinsa/CouponEventApplication.java
+++ coupon-event/src/main/java/com/mosinsa/CouponEventApplication.java
@@ -2,9 +2,11 @@ package com.mosinsa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
+@EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = "com.mosinsa")
 public class CouponEventApplication {
 
