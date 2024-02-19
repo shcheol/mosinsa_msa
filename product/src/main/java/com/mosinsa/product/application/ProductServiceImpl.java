56d2cb803a00b46673f79121acd86b0963559fb9
unknown <kian1523@naver.com>
2024-02-19 18:52:43 +0900

    style

--- product/src/main/java/com/mosinsa/product/application/ProductServiceImpl.java
+++ product/src/main/java/com/mosinsa/product/application/ProductServiceImpl.java
@@ -25,76 +25,75 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
 
-    private final ProductRepository productRepository;
-    private final CategoryService categoryService;
-    private static final String STOCK_LOCK_KEY = "stockLock";
-    private static final String LIKES_LOCK_KEY = "likesLock";
+	private final ProductRepository productRepository;
+	private final CategoryService categoryService;
+	private static final String STOCK_LOCK_KEY = "stockLock";
+	private static final String LIKES_LOCK_KEY = "likesLock";
 
-    @Override
-    public ProductDto createProduct(CreateProductRequest request) {
+	@Override
+	public ProductDto createProduct(CreateProductRequest request) {
 
-        Category category = categoryService.getCategory(request.category());
+		Category category = categoryService.getCategory(request.category());
 
-        return new ProductDto(
-                productRepository.save(
-                        Product.create(request.name(),
-                                request.price(),
-                                category,
-                                request.stock()
-                        )));
-    }
+		return new ProductDto(
+				productRepository.save(
+						Product.create(request.name(),
+								request.price(),
+								category,
+								request.stock())));
+	}
 
-    @Override
-    @Transactional(readOnly = true)
-    public ProductDto getProductById(String productId) {
-        return new ProductDto(productRepository.findById(ProductId.of(productId))
-                .orElseThrow(() -> new ProductException(ProductError.NOT_FOUNT_PRODUCT)));
-    }
+	@Override
+	@Transactional(readOnly = true)
+	public ProductDto getProductById(String productId) {
+		return new ProductDto(productRepository.findById(ProductId.of(productId))
+				.orElseThrow(() -> new ProductException(ProductError.NOT_FOUNT_PRODUCT)));
+	}
 
-    @Override
-    @Transactional(readOnly = true)
-    public Page<ProductDto> getAllProducts(Pageable pageable) {
-        return new PageWrapper<>(productRepository.findAll(pageable).map(ProductDto::new));
-    }
+	@Override
+	@Transactional(readOnly = true)
+	public Page<ProductDto> getAllProducts(Pageable pageable) {
+		return new PageWrapper<>(productRepository.findAll(pageable).map(ProductDto::new));
+	}
 
-    @Override
-    public List<ProductDto> findMyLikesProducts(String customerId) {
-        return productRepository.findLikesProduct(customerId);
-    }
+	@Override
+	public List<ProductDto> findMyLikesProducts(String customerId) {
+		return productRepository.findLikesProduct(customerId);
+	}
 
-    @Override
-    public Page<ProductDto> findProductsByCondition(SearchCondition condition, Pageable pageable) {
-        return productRepository.findByCondition(condition, pageable);
-    }
+	@Override
+	public Page<ProductDto> findProductsByCondition(SearchCondition condition, Pageable pageable) {
+		return productRepository.findByCondition(condition, pageable);
+	}
 
 
-    @Override
-    @RedissonLock(value = STOCK_LOCK_KEY)
-    public void orderProduct(List<OrderProductRequest> requests) {
+	@Override
+	@RedissonLock(value = STOCK_LOCK_KEY)
+	public void orderProduct(List<OrderProductRequest> requests) {
 
-        log.info("order: {}", requests);
-        requests.forEach(request ->
-                productRepository.findById(ProductId.of(request.productId()))
-                        .orElseThrow(() -> new ProductException(ProductError.NOT_FOUNT_PRODUCT))
-                        .decreaseStock(request.quantity()));
+		log.info("order: {}", requests);
+		requests.forEach(request ->
+				productRepository.findById(ProductId.of(request.productId()))
+						.orElseThrow(() -> new ProductException(ProductError.NOT_FOUNT_PRODUCT))
+						.decreaseStock(request.quantity()));
 
-    }
+	}
 
-    @Override
-    @RedissonLock(value = STOCK_LOCK_KEY)
-    public void cancelOrderProduct(List<CancelOrderProductRequest> requests) {
-        log.info("cancelOrder: {}", requests);
-        requests.forEach(request ->
-                productRepository.findById(ProductId.of(request.productId()))
-                        .orElseThrow(() -> new ProductException(ProductError.NOT_FOUNT_PRODUCT))
-                        .increaseStock(request.quantity()));
-    }
+	@Override
+	@RedissonLock(value = STOCK_LOCK_KEY)
+	public void cancelOrderProduct(List<CancelOrderProductRequest> requests) {
+		log.info("cancelOrder: {}", requests);
+		requests.forEach(request ->
+				productRepository.findById(ProductId.of(request.productId()))
+						.orElseThrow(() -> new ProductException(ProductError.NOT_FOUNT_PRODUCT))
+						.increaseStock(request.quantity()));
+	}
 
-    @Override
-    @RedissonLock(value = LIKES_LOCK_KEY)
-    public void likes(LikesProductRequest request) {
-        productRepository.findById(ProductId.of(request.productId()))
-                .orElseThrow(() -> new ProductException(ProductError.NOT_FOUNT_PRODUCT))
-                .likes(request.memberId());
-    }
+	@Override
+	@RedissonLock(value = LIKES_LOCK_KEY)
+	public void likes(LikesProductRequest request) {
+		productRepository.findById(ProductId.of(request.productId()))
+				.orElseThrow(() -> new ProductException(ProductError.NOT_FOUNT_PRODUCT))
+				.likes(request.memberId());
+	}
 }
