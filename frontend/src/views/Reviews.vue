c56b907957cc591516629d2fd6aadfbb631ed44f
shcheol <kian1523@naver.com>
2024-06-22 16:11:40 +0900

    fix

--- frontend/src/views/Reviews.vue
+++ frontend/src/views/Reviews.vue
@@ -116,7 +116,7 @@ export default {
   },
   methods: {
     connect() {
-      const serverURL = "http://localhost:8000/product-service/register"
+      const serverURL = "/product-service/register"
       let socket = new SockJS(serverURL);
       this.stompClient = Stomp.over(socket);
       console.log(`소켓 연결을 시도합니다. 서버 주소: ${serverURL}`)
