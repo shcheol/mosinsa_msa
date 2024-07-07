140d57c4f7959746ddbe0af5f365b09ff297c3f7
shcheol <kian1523@naver.com>
2024-07-07 14:15:15 +0900

    refactor: stock

--- product/src/main/java/com/mosinsa/product/command/domain/StockHistory.java
+++ product/src/main/java/com/mosinsa/product/command/domain/StockHistory.java
@@ -5,14 +5,36 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.EqualsAndHashCode;
 
+import java.util.UUID;
+
 @Entity
-@EqualsAndHashCode(of = "id")
 public class StockHistory {
 
 	@Id
 	@Column(name = "stock_history")
 	private String id;
 
+	private String orderNum;
 
+	private String memberId;
 
+	private String productId;
+
+	private Long quantity;
+
+	private StockHistoryType type;
+
+	protected StockHistory() {
+	}
+
+	public static StockHistory of(String orderNum, String memberId, String productId, Long quantity, StockHistoryType type) {
+		StockHistory stockHistory = new StockHistory();
+		stockHistory.id = UUID.randomUUID().toString();
+		stockHistory.orderNum = orderNum;
+		stockHistory.memberId = memberId;
+		stockHistory.productId = productId;
+		stockHistory.quantity = quantity;
+		stockHistory.type = type;
+		return stockHistory;
+	}
 }
