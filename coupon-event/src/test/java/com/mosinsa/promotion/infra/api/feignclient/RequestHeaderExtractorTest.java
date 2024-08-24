8ad6a65ab73fee5c0c95f0b18823443ced0e1e6e
shcheol <kian1523@naver.com>
2024-08-25 00:24:23 +0900

    test

--- coupon-event/src/test/java/com/mosinsa/promotion/infra/api/feignclient/RequestHeaderExtractorTest.java
+++ coupon-event/src/test/java/com/mosinsa/promotion/infra/api/feignclient/RequestHeaderExtractorTest.java
@@ -5,7 +5,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
 import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -13,12 +12,10 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class RequestHeaderExtractorTest {
 
     @Test
-    void extractEmpty(){
+    void extractEmpty() {
         MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
         ServletRequestAttributes servletRequestAttributes = new ServletRequestAttributes(mockHttpServletRequest);
         RequestContextHolder.setRequestAttributes(servletRequestAttributes);
@@ -27,17 +24,19 @@ class RequestHeaderExtractorTest {
     }
 
     @Test
-    void extract(){
+    void extract() {
         MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
         mockHttpServletRequest.addHeader(HttpHeaders.AUTHORIZATION, "bearer token");
         mockHttpServletRequest.addHeader(HeaderConst.REFRESH_TOKEN.key(), "refresh token");
         mockHttpServletRequest.addHeader(HeaderConst.CUSTOMER_INFO.key(), "customer info");
         ServletRequestAttributes servletRequestAttributes = new ServletRequestAttributes(mockHttpServletRequest);
         RequestContextHolder.setRequestAttributes(servletRequestAttributes);
+
         Map<String, Collection<String>> extract = RequestHeaderExtractor.extract();
-        Assertions.assertThat(extract.get(HttpHeaders.AUTHORIZATION)).isEqualTo(List.of("bearer token"));
-        Assertions.assertThat(extract.get(HeaderConst.REFRESH_TOKEN.key())).isEqualTo(List.of("refresh token"));
-        Assertions.assertThat(extract.get(HeaderConst.CUSTOMER_INFO.key())).isEqualTo(List.of("customer info"));
+
+        Assertions.assertThat(extract).containsEntry(HttpHeaders.AUTHORIZATION, List.of("bearer token"))
+                .containsEntry(HeaderConst.REFRESH_TOKEN.key(), List.of("refresh token"))
+                .containsEntry(HeaderConst.CUSTOMER_INFO.key(), List.of("customer info"));
     }
 
 }
